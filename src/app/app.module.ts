050a0cc2dfc257ebd30c56b5549f4b8e6576f77a
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-11-23 20:28:13 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FuelComponent } from './fuel/fuel.component';
+import { FoodComponent } from './food/food.component';
+import { FunComponent } from './fun/fun.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+	{ path: 'fuel', component: FuelComponent },
+	{ path: 'food', component: FoodComponent },
+  { path: 'fun', component: FunComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuelComponent,
+    FoodComponent,
+    FunComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
